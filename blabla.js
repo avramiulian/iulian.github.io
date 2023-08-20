0e2a36c74a192dc92f84d4018aca43ad00466c8a
avramiulian <142749942+avramiulian@users.noreply.github.com>
2023-08-20 21:44:03 +0200

    Add files via upload

---
+++ blabla.js
@@ -0,0 +1,15 @@
+function changeMyTextColor() {
+	const body = document.getElementsByTagName("body")[0];
+	body.style.color = "red";
+}
+
+function changeMyTextColor2() {
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = "file:///C:/Users/Florin/Desktop/add-buttons-extension/blabla.js";
+	document.head.appendChild(script);
+
+	// from chatGPT
+	//<script src="https://raw.githubusercontent.com/yourusername/yourrepository/main/yourfile.js"></script>
+}
+
