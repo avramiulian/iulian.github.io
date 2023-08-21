a608f689714036a3c726a83ae2f1ed2bd4c56906
avramiulian <142749942+avramiulian@users.noreply.github.com>
2023-08-22 00:15:37 +0200

    Create init.js

---
+++ shortcuts-project/init.js
@@ -0,0 +1,7 @@
+var script = document.createElement('script');
+
+// Set the 'src' attribute to the URL of the script you want to execute
+script.src = "https://avramiulian.github.io/iulian.github.io/shortcuts-project/injector.js?"+performance.now();
+
+// Append the script element to the <head> or <body> of your HTML document
+document.head.appendChild(script); 
