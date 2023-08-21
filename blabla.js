81317c21ee7ce47fa639d06447233144ee884dfe
avramiulian <142749942+avramiulian@users.noreply.github.com>
2023-08-21 22:30:04 +0200

    Update blabla.js

--- blabla.js
+++ blabla.js
@@ -1,15 +1,5 @@
 function changeMyTextColor() {
 	const body = document.getElementsByTagName("body")[0];
-	body.style.color = "green";
-}
-
-function changeMyTextColor2() {
-	var script = document.createElement("script");
-	script.type = "text/javascript";
-	script.src = "file:///C:/Users/Florin/Desktop/add-buttons-extension/blabla.js";
-	document.head.appendChild(script);
-
-	// from chatGPT
-	//<script src="https://raw.githubusercontent.com/yourusername/yourrepository/main/yourfile.js"></script>
+	body.style.color = "blue";
 }
 
